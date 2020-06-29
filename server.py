0eb5f6ecb82fb907d1c3771894e07b715c4909b5
Chinmay Jain <chinmay81098@gmail.com>
2020-06-29 23:21:42 +0530

    second commit

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route('/receiveObstacles',methods=["POST"])
 def index():
-    if request.method == "POST':
+    if request.method == "POST":
         return "kakkar bsdka"
 
 
-if __name__ = '__main__':
-   app.run()
+if __name__ == '__main__':
+    app.run()
 
